f7b30a7db895fccf08b0ed8b53e68b5808c417ae
stijnvancrombrugge <vancrombruggestijn@gmail.com>
2015-10-27 17:24:46 +0100

    Initial commit

---
+++ fleet-web/src/main/java/FleetApplication.java
@@ -0,0 +1,11 @@
+/**
+ * Created by SDOAX36 on 27/10/2015.
+ */
+public class FleetApplication {
+
+
+    public static void main(String[]args)
+    {
+        System.out.println("Start");
+    }
+}
