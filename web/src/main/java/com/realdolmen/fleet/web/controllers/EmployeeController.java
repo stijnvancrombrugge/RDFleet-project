cb5b2ace639c3caf587d34481aa43d55791b983d
stijnvancrombrugge <vancrombruggestijn@gmail.com>
2015-11-09 17:33:32 +0100

    Added up or downgrade and ajax

--- web/src/main/java/com/realdolmen/fleet/web/controllers/EmployeeController.java
+++ web/src/main/java/com/realdolmen/fleet/web/controllers/EmployeeController.java
@@ -16,10 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
@@ -63,19 +60,28 @@ public class EmployeeController {
     }
 
     @RequestMapping(value = {"/employee/home","/employee","/employee/index"}, method = RequestMethod.GET)
-    public String home(){
-        return "/employee/home";
-    }
+    public String home(){ return "/employee/home";}
 
 
     @RequestMapping(value= "/employee/order", method = RequestMethod.GET)
-    public String checkorder(){
-       return "/employee/order";
+    public String checkorder(Model model){
+        model.addAttribute(new CarModelFilterViewModel());
+        return "/employee/order";
+    }
+
+
+    @RequestMapping(value="/employee/checkOrderCode/{code}", method = RequestMethod.GET)
+    @ResponseBody
+    public boolean checkOrderCode(@PathVariable("code") String code){
+        return true;
     }
 
     @RequestMapping(value= "/employee/order", method = RequestMethod.POST)
-    public String startOrder(){
-        return "/employee/mark";
+    public String startOrder(@Valid CarModelFilterViewModel carModelFilterViewModel, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            return "redirect:/employee/order?error";
+        }
+        return "redirect:/employee/" + carModelFilterViewModel.getCategory() + "/" + carModelFilterViewModel.getMark();
     }
 
     @RequestMapping(value="/employee/{category}/{mark}", method = RequestMethod.GET)
